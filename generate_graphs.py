6e05d605c9954a566bb14df8995773ec5d241114
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2024-08-18 00:54:31 -0500

    removed line chart with bar chart in breakdown

--- generate_graphs.py
+++ generate_graphs.py
@@ -1,7 +1,7 @@
-import pandas as pd
-import matplotlib.pyplot as plt
 import os
 import warnings
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def read_markdown_table(file_path, method, replacement_dict):
@@ -24,6 +24,7 @@ def read_markdown_table(file_path, method, replacement_dict):
     df = df.reset_index(drop=True)
     grouped = df.groupby(df.index // 5).agg(
         {col: 'mean' if df[col].dtype != 'object' else 'first' for col in df.columns})
+    grouped["Other"] = grouped['Initialization'] + grouped['Hashtable'] + grouped['Finalization']
     return grouped
 
 
@@ -71,6 +72,69 @@ def show_line_chart(df, figure_name=None, application=None):
     print(f"Figure saved in {figure_path}")
 
 
+def show_breakdown_bar_chart(df, figure_name=None, breakdown_columns=None, application=None):
+    total_time_column = 'Total Time'
+    bar_width = 4  # Adjust this value to make the bars wider (default is 0.8)
+    # Loop through each dataset
+    datasets = df['Dataset'].unique()
+    for dataset in datasets:
+        # Create a new figure and axis
+        fig, ax = plt.subplots()
+        # Filter the subset of data for the current dataset
+        subset = df[df['Dataset'] == dataset]
+        # Plot stacked bar chart for breakdown columns
+        bar_bottom = None
+        for column in breakdown_columns:
+            if bar_bottom is None:
+                bar_bottom = subset[column]
+                ax.bar(subset['# Process'], subset[column], width=bar_width, label=column)
+            else:
+                ax.bar(subset['# Process'], subset[column], bottom=bar_bottom, width=bar_width, label=column)
+                bar_bottom += subset[column]
+
+        # Plot the Total Time as a line chart on the same axis
+        ax.plot(subset['# Process'], subset[total_time_column], color='black', marker='o',
+                label=total_time_column, linewidth=2)
+
+        # Annotate the total time values on top of the points
+        for i, process in enumerate(subset['# Process']):
+            ax.text(process, subset[total_time_column].iloc[i]+ (0.02 * subset[total_time_column].max()),
+                    f'{subset[total_time_column].iloc[i]:.2f}',
+                    ha='center', va='baseline', fontsize=12, color='black')
+
+        # Set titles and labels
+        method = subset["Method"].unique()[0]
+        ax.set_title(f'{method}: {dataset}')
+        ax.set_xlabel('Process Count')
+        ax.set_ylabel('Time (S)')
+
+        # Set X-axis ticks to the unique number of processes
+        unique_processes = sorted(subset['# Process'].unique())
+        ax.set_xticks(unique_processes)
+
+        # Add a single merged legend
+        ax.legend()
+
+        # Adjust the layout
+        plt.tight_layout()
+        figure_path = ""
+        if not figure_name:
+            figure_path = os.path.join("drawing", "breakdown", f"{dataset}.png")
+        else:
+            # Define the directory path
+            directory = os.path.join("drawing", f"{application.lower()}_breakdown")
+            # Create the directory if it doesn't exist
+            os.makedirs(directory, exist_ok=True)
+            figure_path = os.path.join(directory, f"{figure_name}_{dataset}.png")
+
+        # Save the figure
+        fig.savefig(figure_path, bbox_inches="tight")
+        print(f"Figure saved in {figure_path}")
+
+        # Close the figure to avoid too many open figures
+        plt.close(fig)
+
+
 def show_breakdown_line_chart(df, figure_name=None, breakdown_columns=None, application=None):
     # Loop through each dataset and method
     datasets = df['Dataset'].unique()
@@ -105,7 +169,7 @@ def show_breakdown_line_chart(df, figure_name=None, breakdown_columns=None, appl
             figure_path = os.path.join("drawing", "breakdown", f"{dataset}.png")
         else:
             # Define the directory path
-            directory = os.path.join("drawing", f"{application.lower()}_breakdown")
+            directory = os.path.join("drawing", f"{application.lower()}_breakdown_line")
             # Create the directory if it doesn't exist
             os.makedirs(directory, exist_ok=True)
             figure_path = os.path.join(directory, figure_name)
@@ -119,13 +183,9 @@ def show_breakdown_line_chart(df, figure_name=None, breakdown_columns=None, appl
 
 
 def generate_charts(application="TC"):
-    cam_two_pass_file = f"drawing/{application.lower()}_cam_two_pass.md"
-    cam_sort_file = f"drawing/{application.lower()}_cam_sort.md"
-    mpi_cpu_two_pass_file = f"drawing/{application.lower()}_traditional_two_pass.md"
-    mpi_cpu_sort_file = f"drawing/{application.lower()}_traditional_sort.md"
+    breakdown_columns = ['Join', 'Buffer preparation',
+                         'Communication', 'Deduplication', 'Merge', 'Other']
     replacement_dict = None
-    breakdown_columns = ['Total Time', 'Initialization', 'Hashtable', 'Join', 'Buffer preparation',
-                         'Communication', 'Deduplication', 'Merge', 'Finalization']
     if application == "TC":
         row_numbers = ["409,593", "165,435", "147,892", "1,049,866", "552,020"]
         dataset_names = ["fe_ocean, 247, 1669M", "usroad, 606, 871M", "p2p-Gnutella31, 31, 884M", "com-dblp, 31, 1911M",
@@ -136,6 +196,10 @@ def generate_charts(application="TC"):
         dataset_names = ["fe_ocean, 77, 65M", "usroad, 588, 3137M", "p2p-Gnutella31, 20, 3700M", "fe_body, 40, 408M",
                          "vsp_finan, 513, 864M"]
         replacement_dict = dict(zip(row_numbers, dataset_names))
+    cam_two_pass_file = f"drawing/{application.lower()}_cam_two_pass.md"
+    cam_sort_file = f"drawing/{application.lower()}_cam_sort.md"
+    mpi_cpu_two_pass_file = f"drawing/{application.lower()}_traditional_two_pass.md"
+    mpi_cpu_sort_file = f"drawing/{application.lower()}_traditional_sort.md"
 
     cam_sort_df = read_markdown_table(cam_sort_file, "CUDA Aware MPI - Sort", replacement_dict)
     cam_two_pass_df = read_markdown_table(cam_two_pass_file, "CUDA Aware MPI - Two pass", replacement_dict)
@@ -159,10 +223,10 @@ def generate_charts(application="TC"):
     show_line_chart(sort_df, "cuda_aware_mpi_sort_vs_traditional_mpi_sort.png", application)
 
     # Breakdown charts
-    show_breakdown_line_chart(cam_two_pass_df, "cuda_aware_mpi_two_pass", breakdown_columns, application)
-    show_breakdown_line_chart(cam_sort_df, "cuda_aware_mpi_sort", breakdown_columns, application)
-    show_breakdown_line_chart(mpi_cpu_two_pass_df, "traditional_mpi_two_pass", breakdown_columns, application)
-    show_breakdown_line_chart(mpi_cpu_sort_df, "traditional_mpi_sort", breakdown_columns, application)
+    show_breakdown_bar_chart(cam_two_pass_df, "cuda_aware_mpi_two_pass", breakdown_columns, application)
+    show_breakdown_bar_chart(cam_sort_df, "cuda_aware_mpi_sort", breakdown_columns, application)
+    show_breakdown_bar_chart(mpi_cpu_two_pass_df, "traditional_mpi_two_pass", breakdown_columns, application)
+    show_breakdown_bar_chart(mpi_cpu_sort_df, "traditional_mpi_sort", breakdown_columns, application)
 
 
 if __name__ == "__main__":
