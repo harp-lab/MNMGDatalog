9de67f76cff6ab282681bc0fde0b76aa17f8b80e
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2024-12-30 23:15:13 -0600

    added chunked scan for thrust

--- common/join.cu
+++ common/join.cu
@@ -50,13 +50,10 @@ void get_join_result_entity(Entity *hash_table, int hash_table_size,
 
 Entity *get_join(int grid_size, int block_size, Entity *hash_table, int hash_table_size, Entity *relation,
                  int relation_size, int *join_result_size, double *compute_time) {
-//    show_variable_generic(hash_table, "hash_table", hash_table_size, "Entity", "device", 0, 0, "hash table in get join", 0);
-//    show_variable_generic(relation, "relation", relation_size, "Entity", "device", 0, 0, "", 0);
     double start_time, end_time, elapsed_time;
     start_time = MPI_Wtime();
     Entity *join_result = nullptr;
     if (hash_table_size == 0) {
-//        checkCuda(cudaMalloc((void **) &join_result, 0));
         *join_result_size = 0;
         end_time = MPI_Wtime();
         elapsed_time = end_time - start_time;
@@ -70,17 +67,26 @@ Entity *get_join(int grid_size, int block_size, Entity *hash_table, int hash_tab
 
     get_join_result_size_entity<<<grid_size, block_size>>>(hash_table, hash_table_size,
                                                            relation, relation_size, join_offset);
-    cudaDeviceSynchronize();
+//    checkCuda(cudaDeviceSynchronize());
     result_size = thrust::reduce(thrust::device, join_offset, join_offset + relation_size, 0, thrust::plus<int>());
-    thrust::exclusive_scan(thrust::device, join_offset, join_offset + relation_size, join_offset);
+    int step = 90'000'000;
+    for (int i = 0; i < relation_size; i += step) {
+        int end = i + step;
+        if (i + step >= relation_size) {
+            end = relation_size - i;
+        }
+        thrust::exclusive_scan(thrust::device, join_offset + i, join_offset + end, join_offset + i);
+    }
+
+    //    thrust::exclusive_scan(thrust::device, join_offset, join_offset + relation_size, join_offset);
 #ifdef DEBUG
     cout << "result_size * sizeof(Entity): " << result_size * sizeof(Entity) << endl;
 #endif
-
+//    checkCuda(cudaDeviceSynchronize());
     checkCuda(cudaMalloc((void **) &join_result, result_size * sizeof(Entity)));
     get_join_result_entity<<<grid_size, block_size>>>(hash_table, hash_table_size,
                                                       relation, relation_size, join_offset, join_result);
-    cudaDeviceSynchronize();
+//    checkCuda(cudaDeviceSynchronize());
     cudaFree(join_offset);
     *join_result_size = result_size;
     end_time = MPI_Wtime();
